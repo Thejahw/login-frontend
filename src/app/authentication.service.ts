52fb7fb45a9caf7b4404a39e313430c8cdb82042
Theja <thejahw@gmail.com>
2020-07-10 09:34:57 +0530

    Value change

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -28,7 +28,7 @@ export class AuthenticationService {
     return this.http.post<any>(`${this.apibase}login`, { username, password })
         .pipe(map(user => {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('currentUser', user);
+            localStorage.setItem('currentUser', user); 
             // this.currentUserSubject.next(user);
             // console.log("current: ",localStorage.getItem('currentUser'));
             return user;
